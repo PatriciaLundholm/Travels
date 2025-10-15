b2cc1ce796a5f9b949ed395b96800bf8cc57fb49
patiipoot <tatersgonatate@hotmail.com>
2025-10-15 11:44:29 +0200

    Destination repository

--- src/main/java/com/Group3/Travels/Repository/DestinationRepository.java
+++ src/main/java/com/Group3/Travels/Repository/DestinationRepository.java
@@ -2,6 +2,8 @@ package com.Group3.Travels.Repository;
 
 import com.Group3.Travels.Entity.Destination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DestinationRepository extends JpaRepository<Destination, Long> {
 }

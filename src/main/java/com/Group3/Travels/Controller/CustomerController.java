564f3f4d71a62aaa21ce566a2761966fe63f1897
patiipoot <tatersgonatate@hotmail.com>
2025-10-15 12:18:57 +0200

    Skapat klass för Customerrepository och customercontroller så jag kan kolla om det funkaar över postman, Har inte kollat ännu

---
+++ src/main/java/com/Group3/Travels/Controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.Group3.Travels.Controller;
+
+
+import com.Group3.Travels.Entity.Customer;
+import com.Group3.Travels.Repository.CustomerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    private final CustomerRepository customerRepository;
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
---
+++ src/main/java/com/Group3/Travels/Repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.Group3.Travels.Repository;
+
+
+import com.Group3.Travels.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
